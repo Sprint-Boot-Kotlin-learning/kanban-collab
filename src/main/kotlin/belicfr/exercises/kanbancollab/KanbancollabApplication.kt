b09eb98627eac3087ce5532aae734c2b8deacdf2
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-10 15:08:41 +0200

    Project installation

---
+++ src/main/kotlin/belicfr/exercises/kanbancollab/KanbancollabApplication.kt
@@ -0,0 +1,11 @@
+package belicfr.exercises.kanbancollab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KanbancollabApplication
+
+fun main(args: Array<String>) {
+	runApplication<KanbancollabApplication>(*args)
+}
---
+++ src/test/kotlin/belicfr/exercises/kanbancollab/KanbancollabApplicationTests.kt
@@ -0,0 +1,13 @@
+package belicfr.exercises.kanbancollab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KanbancollabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
