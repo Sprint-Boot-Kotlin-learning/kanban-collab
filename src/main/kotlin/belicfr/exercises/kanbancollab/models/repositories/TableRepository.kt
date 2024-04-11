bfee0ae73de713d930c24382406371d98962d3d7
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 13:40:21 +0200

    feat(User): change repository implement for Jpa
    + add findAllByMembersContainsOrderByIdDesc() method

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/TableRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/TableRepository.kt
@@ -1,7 +1,12 @@
 package belicfr.exercises.kanbancollab.models.repositories
 
 import belicfr.exercises.kanbancollab.models.KTable
+import belicfr.exercises.kanbancollab.models.KUser
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface TableRepository: CrudRepository<KTable, Long> {
+interface TableRepository: JpaRepository<KTable, Long> {
+
+    fun findAllByMembersContainsOrderByIdDesc(member: KUser): List<KTable>
+
 }
