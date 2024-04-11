5f4b25ccd62d41271a89912b786dbf9405909377
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 13:39:55 +0200

    feat(User): change repository implement for Jpa

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/UserRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/UserRepository.kt
@@ -1,9 +1,10 @@
 package belicfr.exercises.kanbancollab.models.repositories
 
 import belicfr.exercises.kanbancollab.models.KUser
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface UserRepository: CrudRepository<KUser, Long> {
+interface UserRepository: JpaRepository<KUser, Long> {
 
     fun countAllByLogin(login: KUser.Username): Int
 
