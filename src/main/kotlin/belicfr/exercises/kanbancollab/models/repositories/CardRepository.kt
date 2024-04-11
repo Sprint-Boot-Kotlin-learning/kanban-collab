a0e9b6176ae187d1d80d4ceed0c34cb1ed0462a5
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 13:40:39 +0200

    feat(Card): change repository implement for Jpa

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/CardRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/CardRepository.kt
@@ -1,7 +1,8 @@
 package belicfr.exercises.kanbancollab.models.repositories
 
 import belicfr.exercises.kanbancollab.models.KCard
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface CardRepository: CrudRepository<KCard, Long> {
+interface CardRepository: JpaRepository<KCard, Long> {
 }
