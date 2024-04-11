c5477ee167d3859819126b70bdb394b94304b513
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 14:29:18 +0200

    feat(Table): add findKTableByToken() and countAllByToken() methods

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/TableRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/TableRepository.kt
@@ -4,9 +4,14 @@ import belicfr.exercises.kanbancollab.models.KTable
 import belicfr.exercises.kanbancollab.models.KUser
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
+import java.util.UUID
 
 interface TableRepository: JpaRepository<KTable, Long> {
 
     fun findAllByMembersContainsOrderByIdDesc(member: KUser): List<KTable>
+
+    fun findKTableByToken(token: UUID): KTable?
+
+    fun countAllByToken(token: UUID): Int
 
 }
