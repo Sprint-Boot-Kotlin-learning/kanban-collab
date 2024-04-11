732c271e89ed8497d2ad93a754f4461af1bb6ce6
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 13:36:51 +0200

    feat(Table): set null as id default value
    + set members and lists as mutable
    + add getSomeMembers() method

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/KTable.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/KTable.kt
@@ -1,16 +1,20 @@
 package belicfr.exercises.kanbancollab.models
 
+import jakarta.persistence.Embeddable
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToMany
 import jakarta.persistence.OneToMany
+import java.util.UUID
 
 @Entity
-class KTable(@Id @GeneratedValue val id: Long?,
+class KTable(@Id @GeneratedValue val id: Long? = null,
              var name: String,
-             @ManyToMany val members: Set<KUser>,
-             @OneToMany val lists: Set<KList>) {
+             val token: UUID = UUID.randomUUID(),
+             @ManyToMany val members: MutableSet<KUser> = mutableSetOf(),
+             @OneToMany val lists: MutableSet<KList> = mutableSetOf()) {
 
-    
+    fun getSomeMembers(count: Int): List<KUser>
+        = this.members.take(count)
 }
