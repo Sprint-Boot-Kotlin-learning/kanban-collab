0389570c781e3d629e7067380dc3d720c2aed156
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 15:54:40 +0200

    feat(List): add some static constants and utilities methods

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/KList.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/KList.kt
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.OneToMany
-import jakarta.persistence.Table
 
 @Entity
 class KList(@Id @GeneratedValue val id: Long? = null,
@@ -14,5 +13,20 @@ class KList(@Id @GeneratedValue val id: Long? = null,
             @ManyToOne val table: KTable,
             @OneToMany val cards: MutableSet<KCard> = mutableSetOf()) {
 
+    companion object {
+        const val NAME_MAX_LENGTH = 50
 
+        const val INVALID_NAME_LENGTH_ERROR
+            = "The name must contain less than " +
+              "$NAME_MAX_LENGTH characters."
+
+        const val INVALID_POSITION_ERROR
+            = "The position must be greater than 0."
+
+        fun isNameValid(name: String): Boolean
+            = name.length <= NAME_MAX_LENGTH
+
+        fun isPositionValid(position: Int): Boolean
+            = position > 0
+    }
 }
