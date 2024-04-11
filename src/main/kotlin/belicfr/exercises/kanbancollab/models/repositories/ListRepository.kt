b8083be0c3fced2d6d5a91488a0ce5195c0be53d
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 15:01:46 +0200

    feat(List): add some methods to repository
    + findAllByOrderByPosition()
    + findFirstByTableOrderByIdDesc()
    + countAllByTable()

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/ListRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/ListRepository.kt
@@ -1,8 +1,16 @@
 package belicfr.exercises.kanbancollab.models.repositories
 
 import belicfr.exercises.kanbancollab.models.KList
+import belicfr.exercises.kanbancollab.models.KTable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
 interface ListRepository: JpaRepository<KList, Long> {
+
+    fun findAllByOrderByPosition(): List<KList>
+
+    fun findFirstByTableOrderByIdDesc(table: KTable): KList?
+
+    fun countAllByTable(table: KTable): Int
+
 }
