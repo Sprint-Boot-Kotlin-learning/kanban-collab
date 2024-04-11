a858cf3adb821887b0922a8e78142c3ad8ca6c04
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 16:53:41 +0200

    feat(Board): add new project methods and mappings and setup method
    using middleware class

--- src/main/kotlin/belicfr/exercises/kanbancollab/controllers/board/ProjectController.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/controllers/board/ProjectController.kt
@@ -1,5 +1,6 @@
 package belicfr.exercises.kanbancollab.controllers.board
 
+import belicfr.exercises.kanbancollab.controllers.Middleware
 import belicfr.exercises.kanbancollab.controllers.auth.AuthController
 import belicfr.exercises.kanbancollab.models.KList
 import belicfr.exercises.kanbancollab.models.KTable
@@ -11,11 +12,7 @@ import jakarta.servlet.http.HttpSession
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import org.springframework.web.servlet.view.RedirectView
 import java.util.UUID
@@ -23,14 +20,19 @@ import java.util.UUID
 @Controller
 @RequestMapping("/board/project")
 class ProjectController(private val tableRepository: TableRepository,
-                        private val listRepository: ListRepository) {
+                        private val listRepository: ListRepository,
+                        private val session: HttpSession) {
 
     private lateinit var project: KTable
 
+    @ModelAttribute
+    fun setup(model: Model) {
+        Middleware.redirectIfNotLogged(session)
+    }
+
     @GetMapping("/{token}", "/{token}/")
     fun renderProject(@PathVariable("token") token: UUID,
-                      model: Model,
-                      session: HttpSession): String {
+                      model: Model): String {
 
         if (!this.isProjectExisting(token)) {
             return "redirect:/board"
@@ -53,7 +55,8 @@ class ProjectController(private val tableRepository: TableRepository,
 
         model["user"] = user
         model["project"] = project
-        model["orderedLists"] = listRepository.findAllByOrderByPosition()
+        model["orderedLists"] = listRepository.findAllByTableOrderByPosition(
+            this.project)
         model["nextPosition"] = nextPosition
 
         return "Project"
@@ -107,6 +110,51 @@ class ProjectController(private val tableRepository: TableRepository,
         }
 
         return Redirect.to("/board/project/$token")
+    }
+
+    @GetMapping("/new", "/new/")
+    fun renderNewProject(model: Model): String {
+
+        val user: KUser = session.getAttribute("user") as KUser
+
+        model["user"] = user
+        model["nameMaxLength"] = KTable.NAME_MAX_LENGTH
+
+        return "NewProject"
+    }
+
+    @PostMapping("/new/create", "/new/create/")
+    fun createProject(@RequestParam("name") name: String,
+                      redirectAttributes: RedirectAttributes): RedirectView {
+
+        val errors: MutableList<String> = arrayListOf()
+
+        val user: KUser = session.getAttribute("user") as KUser
+
+        if (name.isBlank()) {
+            errors.add(String.format(AuthController.EMPTY_REQUIRED_FIELD_ERROR,
+                                     "Name"))
+        }
+
+        if (!KTable.isNameValid(name)) {
+            errors.add(KTable.INVALID_NAME_LENGTH_ERROR)
+        }
+
+        if (errors.isNotEmpty()) {
+            redirectAttributes.addFlashAttribute("errors",
+                                                 errors)
+
+            return Redirect.to("/board/project/new")
+        }
+
+        val project: KTable = KTable(
+            name = name,
+            members = mutableSetOf(user))
+
+        tableRepository.save(project)
+        tableRepository.flush()
+
+        return Redirect.to("/board")
     }
 
     private fun incrementNextLists(list: KList) {
