a2941545e8fa44dd954ff68918cae97f117242ef
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 13:40:32 +0200

    feat(List): change repository implement for Jpa

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/ListRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/ListRepository.kt
@@ -1,7 +1,8 @@
 package belicfr.exercises.kanbancollab.models.repositories
 
 import belicfr.exercises.kanbancollab.models.KList
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface ListRepository: CrudRepository<KList, Long> {
+interface ListRepository: JpaRepository<KList, Long> {
 }
