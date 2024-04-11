cacdef7d5f706572214f7d07ce316048c325841c
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-11 16:01:17 +0200

    feat(Board): add list creation method and mapping

--- src/main/kotlin/belicfr/exercises/kanbancollab/controllers/board/ProjectController.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/controllers/board/ProjectController.kt
@@ -1,17 +1,23 @@
 package belicfr.exercises.kanbancollab.controllers.board
 
+import belicfr.exercises.kanbancollab.controllers.auth.AuthController
 import belicfr.exercises.kanbancollab.models.KList
 import belicfr.exercises.kanbancollab.models.KTable
 import belicfr.exercises.kanbancollab.models.KUser
 import belicfr.exercises.kanbancollab.models.repositories.ListRepository
 import belicfr.exercises.kanbancollab.models.repositories.TableRepository
+import belicfr.exercises.kanbancollab.utilities.Redirect
 import jakarta.servlet.http.HttpSession
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.web.servlet.view.RedirectView
 import java.util.UUID
 
 @Controller
@@ -51,6 +57,54 @@ class ProjectController(private val tableRepository: TableRepository,
         model["nextPosition"] = nextPosition
 
         return "Project"
+    }
+
+    @PostMapping("/lists/create")
+    fun createList(@RequestParam("token") token: UUID,
+                   @RequestParam("name") name: String,
+                   @RequestParam("position") position: Int,
+                   redirectAttributes: RedirectAttributes): RedirectView {
+
+        if (!this.isProjectExisting(token)) {
+            return Redirect.to("/board")
+
+        }
+
+        val errors: MutableList<String> = arrayListOf()
+
+        this.project = tableRepository.findKTableByToken(token) as KTable
+
+        if (name.isBlank()) {
+            errors.add(String.format(AuthController.EMPTY_REQUIRED_FIELD_ERROR,
+                                     "Name"))
+        }
+
+        if (!KList.isNameValid(name)) {
+            errors.add(KList.INVALID_NAME_LENGTH_ERROR)
+        }
+
+        if (!KList.isPositionValid(position)) {
+            errors.add(KList.INVALID_POSITION_ERROR)
+        }
+
+        if (errors.isNotEmpty()) {
+            redirectAttributes.addFlashAttribute("errors",
+                                                 errors)
+        } else {
+            val list: KList = KList(
+                name = name,
+                position = position,
+                table = project)
+
+            listRepository.save(list)
+            listRepository.flush()
+
+            project.lists.add(list)
+            tableRepository.save(project)
+            tableRepository.flush()
+        }
+
+        return Redirect.to("/board/project/$token")
     }
 
     private fun isProjectExisting(token: UUID): Boolean {
