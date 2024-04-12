6e59c68b512970868fcd5b2b5647ea64abd0d81e
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-12 13:17:28 +0200

    feat(Card): add findAllByListOrderByIdDesc() method to repository

--- src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/CardRepository.kt
+++ src/main/kotlin/belicfr/exercises/kanbancollab/models/repositories/CardRepository.kt
@@ -1,8 +1,12 @@
 package belicfr.exercises.kanbancollab.models.repositories
 
 import belicfr.exercises.kanbancollab.models.KCard
+import belicfr.exercises.kanbancollab.models.KList
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
 interface CardRepository: JpaRepository<KCard, Long> {
+
+    fun findAllByListOrderByIdDesc(list: KList): List<KCard>
+
 }
